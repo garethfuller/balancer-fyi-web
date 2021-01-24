a3c8109250ab32c1042acfe5ee8d3836f0ec7ab3
Gareth Fuller <gareth.a.f@gmail.com>
2021-01-24 13:22:46 +0000

    Limit purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: {
+    layers: ['components'],
+    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}']
+  },
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
