ddcc06c966d2f8f9377580bc00a2261f9561e269
Gareth Fuller <gareth.a.f@gmail.com>
2021-01-25 14:11:09 +0000

    Fetch prices on load

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,11 +3,12 @@
     class="bg-white text-gray-700 dark:bg-gray-900 dark:text-gray-300 min-h-screen"
   >
     <AppNav />
-    <Nuxt />
+    <Nuxt v-if="!loading" />
     <AppFooter />
   </div>
 </template>
 <script lang="ts">
+import { mapActions } from 'vuex'
 import AppNav from '~/components/navs/AppNav.vue'
 import AppFooter from '~/components/navs/AppFooter.vue'
 
@@ -17,10 +18,35 @@ export default {
     AppFooter
   },
 
+  data () {
+    return {
+      loading: true
+    }
+  },
+
   head: {
     bodyAttrs: {
       class: 'dark min-h-screen'
     }
+  },
+
+  async beforeMount () {
+    try {
+      const { tokens } = this.$ethConfig
+      const tokenIds = Object.keys(tokens)
+        .map(address => tokens[address].id)
+        .filter(id => !!id)
+      await this.getPrices(tokenIds)
+      this.loading = false
+    } catch (error) {
+      console.error(error)
+    }
+  },
+
+  methods: {
+    ...mapActions({
+      getPrices: 'prices/getAll'
+    })
   }
 }
 </script>
