24ad66b4882213ce14968a4b59719a251194ef3e
Gareth Fuller <gareth.a.f@gmail.com>
2021-01-25 14:13:13 +0000

    Add pool list to home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,43 @@
 <template>
   <div>
-    <h1>Hello world</h1>
+    <PoolList :pools="pools" />
   </div>
 </template>
 
 <script lang="ts">
-export default {}
+import Vue from 'vue'
+import { mapActions, mapState } from 'vuex'
+import PoolList from '~/components/lists/pool_list/PoolList.vue'
+
+export default Vue.extend({
+  components: {
+    PoolList
+  },
+
+  data () {
+    return {
+      loading: true
+    }
+  },
+
+  computed: {
+    ...mapState({
+      pools: state => state.pools.all
+    })
+  },
+
+  async beforeMount () {
+    await this.getPools()
+    console.log(this.pools)
+    this.loading = false
+  },
+
+  methods: {
+    ...mapActions({
+      getPools: 'pools/getAll'
+    })
+  }
+})
 </script>
 
 <style></style>
