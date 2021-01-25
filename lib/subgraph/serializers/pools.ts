1a21327dc14efe387184555cedf31a19eb4e90f1
Gareth Fuller <gareth.a.f@gmail.com>
2021-01-25 15:01:44 +0000

    Pass in ethConfig

--- lib/subgraph/serializers/pools.ts
+++ lib/subgraph/serializers/pools.ts
@@ -1,10 +1,10 @@
 import { getAddress } from '@ethersproject/address'
 import serializeToken from './tokens'
-import { Pool } from '~/types'
+import { Pool, EthConfigObj } from '~/types'
 import { recentSwapVolumeFor } from '~/lib/balancer/poolHelpers'
 
-export default (pool: Pool) : Pool => {
-  pool.tokens = pool.tokens.map(token => serializeToken(token, pool))
+export default (pool: Pool, ethConfig: EthConfigObj) : Pool => {
+  pool.tokens = pool.tokens.map(token => serializeToken(token, pool, ethConfig))
   pool.tokensList = pool.tokensList.map(token => getAddress(token))
   pool.recentSwapVolume = recentSwapVolumeFor(pool)
   return pool
