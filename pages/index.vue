e5c05634280ee1b0fc874584a0b3749c261d0198
Gareth Fuller <gareth.a.f@gmail.com>
2021-01-26 22:30:01 +0000

    Add loading state

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,17 @@
 <template>
   <main>
-    <PoolList v-if="!loading" :pools="pools" @loadMore="loadMorePools" />
+    <BaseGrid v-if="loading">
+      <BaseGridItem width="full" class="md:w-1/2 lg:w-1/3">
+        <div class="loading-block shimmer" />
+      </BaseGridItem>
+      <BaseGridItem width="full" class="md:w-1/2 lg:w-1/3">
+        <div class="loading-block shimmer" />
+      </BaseGridItem>
+      <BaseGridItem width="full" class="md:w-1/2 lg:w-1/3">
+        <div class="loading-block shimmer" />
+      </BaseGridItem>
+    </BaseGrid>
+    <PoolList v-else :pools="pools" @loadMore="loadMorePools" />
   </main>
 </template>
 
@@ -78,4 +89,20 @@ export default Vue.extend({
 })
 </script>
 
-<style></style>
+<style>
+.loading-block {
+  @apply cursor-pointer h-full rounded flex items-center justify-center;
+  min-height: 300px;
+}
+
+@keyframes shimmerBackground {
+  0% {background-position:-5000px 0}
+  100% {background-position:5000px 0}
+}
+
+.shimmer {
+  animation : shimmerBackground 10s infinite;
+  background: linear-gradient(to right, #F9FAFB 4%, #F3F4F6 25%, #F9FAFB 36%);
+  background-size: 1000px 100%;
+}
+</style>
