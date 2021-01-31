8b8f3fc391a60612a918159090e44eb3d9ad0c7f
Gareth Fuller <gareth.a.f@gmail.com>
2021-01-31 18:51:49 +0000

    Refactor pool subgraph queries

--- lib/subgraph/queries/pools.ts
+++ lib/subgraph/queries/pools.ts
@@ -1,126 +1,66 @@
+import smartPools from '@balancer-labs/assets/data/pools.json'
+
 const now = Math.round(new Date().getTime() / 1000)
 const tsYesterday = now - 24 * 3600
+// @ts-ignore
+const smartPoolIds = Object.entries(smartPools).filter(crp => crp[1].is_compatible).map(crp => crp[0].toLowerCase())
 
-type Options = {
-  count?: number,
-  skip?: number
+export type PoolArgs = {
+  where: {
+    active?: boolean,
+    tokensCount_gt?: number,
+    finalized?: boolean
+    crp?: boolean,
+    id_in?: string[]
+  },
+  first: number,
+  skip: number,
+  orderBy: string,
+  orderDirection: string
 }
 
-export const byLiquidityQuery = (options: Options = { count: 20, skip: 0 }) : object => ({
-  query: {
-    pools: {
-      __args: {
+export const defaultArgs: PoolArgs = {
+  where: {
+    active: true,
+    tokensCount_gt: 1,
+    finalized: true
+  },
+  first: 20,
+  skip: 0,
+  orderBy: 'liquidity',
+  orderDirection: 'desc'
+}
+
+export const argsFor = (type: string = 'shared') : PoolArgs => {
+  switch (type) {
+    case 'shared':
+      return defaultArgs
+    case 'smart':
+      return Object.assign({}, defaultArgs, {
         where: {
           active: true,
           tokensCount_gt: 1,
-          finalized: true,
-          tokensList_not: []
-        },
-        first: options.count,
-        skip: options.skip,
-        orderBy: 'liquidity',
-        orderDirection: 'desc'
-      },
-      id: true,
-      publicSwap: true,
-      finalized: true,
-      crp: true,
-      rights: true,
-      swapFee: true,
-      totalWeight: true,
-      totalShares: true,
-      totalSwapVolume: true,
-      liquidity: true,
-      tokensList: true,
-      swapsCount: true,
-      tokens: {
-        __args: {
-          orderBy: 'denormWeight',
-          orderDirection: 'desc'
-        },
-        id: true,
-        address: true,
-        balance: true,
-        decimals: true,
-        symbol: true,
-        denormWeight: true
-      },
-      swaps: {
-        __args: {
-          first: 1,
-          orderBy: 'timestamp',
-          orderDirection: 'desc',
-          where: {
-            timestamp_lt: tsYesterday
-          }
-        },
-        poolTotalSwapVolume: true
-      }
-    }
-  }
-})
-
-export const byIdsQuery = (ids: string[], options: Options = { count: 100 }) : object => ({
-  query: {
-    pools: {
-      __args: {
-        where: {
-          active: true,
-          tokensCount_gt: 1,
-          finalized: true,
-          tokensList_not: [],
-          id_in: ids
-        },
-        first: options.count,
-        skip: 0
-      },
-      id: true,
-      publicSwap: true,
-      finalized: true,
-      crp: true,
-      rights: true,
-      swapFee: true,
-      totalWeight: true,
-      totalShares: true,
-      totalSwapVolume: true,
-      liquidity: true,
-      tokensList: true,
-      swapsCount: true,
-      tokens: {
-        __args: {
-          orderBy: 'denormWeight',
-          orderDirection: 'desc'
-        },
-        id: true,
-        address: true,
-        balance: true,
-        decimals: true,
-        symbol: true,
-        denormWeight: true
-      },
-      swaps: {
-        __args: {
-          first: 1,
-          orderBy: 'timestamp',
-          orderDirection: 'desc',
-          where: {
-            timestamp_lt: tsYesterday
-          }
-        },
-        poolTotalSwapVolume: true
-      }
-    }
-  }
-})
-
-export const byControllerQuery = (proxyAddress: string) : object => ({
-  query: {
-    pools: {
-      __args: {
-        where: {
-          crpController: proxyAddress
+          id_in: smartPoolIds
         }
-      },
+      })
+    case 'private':
+      return Object.assign({}, defaultArgs, {
+        where: {
+          active: true,
+          tokensCount_gt: 1,
+          finalized: false,
+          crp: false
+        }
+      })
+    default:
+      throw new Error('Arg type not handled')
+  }
+}
+
+export default (args: PoolArgs) : Object => ({
+  query: {
+    pools: {
+      __args: args,
       id: true,
       publicSwap: true,
       finalized: true,
